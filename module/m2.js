78bf4b11d5bfb969f7437958444e6ae73886cc04
Amit chauhan <amitpoly2020@gmail.com>
2023-02-05 12:11:53 +0530

    first commit

---
+++ module/m2.js
@@ -0,0 +1,8 @@
+import { firstname,lastname,fun1,fun2,test} from "./m1.js";
+alert(firstname)
+alert(lastname)
+alert(fun1())
+alert(fun2())
+var obj=new test
+ 
+
---
+++ module/mod1.js
@@ -0,0 +1,10 @@
+var fistname="ROHAN"
+var laststname="SONI"
+var age=45 
+function fun1(){
+    console.log("I'm from fun1 mod1")
+}
+function fun2(x,y){
+   return x+y
+}
+export {fistname,laststname,age,fun1,fun2}
